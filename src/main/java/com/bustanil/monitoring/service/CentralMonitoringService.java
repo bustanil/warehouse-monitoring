d41b0c9c6cf2e6cc7895cfe0161b37ba479411a5
Bustanil Arifin <bustanil@protonmail.com>
2025-05-23 22:00:35 +0800

    central monitoring service initial commit

---
+++ src/main/java/com/bustanil/monitoring/service/CentralMonitoringService.java
@@ -0,0 +1,62 @@
+package com.bustanil.monitoring.service;
+
+import com.bustanil.shared.domain.MeasurementReceived;
+import com.bustanil.shared.domain.SensorMeasurement.SensorType;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CentralMonitoringService {
+
+    @Value("${monitoring.sensors.temperature.threshold}")
+    private Double temperatureThreshold;
+    @Value("${monitoring.sensors.humidity.threshold}")
+    private Double humidityThreshold;
+    final ReactiveKafkaConsumerTemplate<String, MeasurementReceived> kafkaConsumerTemplate;
+
+    public static final Logger logger = LoggerFactory.getLogger(CentralMonitoringService.class);
+
+    public CentralMonitoringService(ReactiveKafkaConsumerTemplate<String, MeasurementReceived> kafkaConsumerTemplate) {
+        this.kafkaConsumerTemplate = kafkaConsumerTemplate;
+    }
+
+    @PostConstruct
+    public void startMonitoring() {
+        kafkaConsumerTemplate
+                .receiveAutoAck()
+                .map(record -> record.value())
+                .onErrorContinue((t, o) -> {
+                    logger.error("Error occurred while monitoring", t);
+                })
+                .subscribe(
+                        measurementReceived -> {
+                            logger.info("Received measurement received: {}", measurementReceived);
+                            SensorType sensorType = measurementReceived.sensorMeasurement().getSensorType();
+                            switch (sensorType) {
+                                case HUMIDITY:
+                                    checkThresholdAndAlarm(measurementReceived, humidityThreshold);
+                                    break;
+                                case TEMPERATURE:
+                                    checkThresholdAndAlarm(measurementReceived, temperatureThreshold);
+                                    break;
+                                default:
+                                    logger.error("Unknown sensor type: {}", sensorType);
+                                    break;
+                            }
+                        },
+                        t -> logger.error("Error occurred while monitoring", t)
+                );
+    }
+
+    private void checkThresholdAndAlarm(MeasurementReceived measurementReceived, Double temperatureThreshold) {
+        if (measurementReceived.sensorMeasurement().getValue() > temperatureThreshold) {
+            logger.warn("⚠\uFE0F {} threshold exceeded at warehouse {}, {} > {}",
+                    measurementReceived.sensorMeasurement().getSensorType().toString(), measurementReceived.warehouseId(), measurementReceived.sensorMeasurement().getValue(), temperatureThreshold);
+        }
+    }
+}
